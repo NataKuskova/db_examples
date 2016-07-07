bde444a3af1490ad44e1899de9e8f30f39894ee2
Natali <natasha.kuskova@gmail.com>
2016-07-07 16:37:14 +0300

    new_sql_query

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import MySQLdb.cursors
 
 def get_connection():
     connection = MySQLdb.connect(user='root',
-                                 passwd='1234',
-                                 db='books',
+                                 passwd='123',
+                                 db='base_example',
                                  cursorclass=MySQLdb.cursors.DictCursor)
     return connection
 
@@ -16,10 +16,20 @@ def insert_books(values):
     cursor.executemany("INSERT INTO Books (title, ISBN) VALUES (%s, %s)", values)
 
 
-def select():
+def select(**kwargs):
     connection = get_connection()
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM Books")
+    sql = "SELECT * FROM Books"
+    sql_where = ''
+    if kwargs:
+        sql += " Where "
+        for name in kwargs.items():
+            if not sql_where:
+                sql_where = name[0] + "=" + str(name[1])
+            else:
+                sql_where += " and " + name[0] + "=" + str(name[1])
+        sql += sql_where
+    cursor.execute(sql)
     return cursor.fetchall()
 
 
@@ -29,4 +39,4 @@ def select():
 # ])
 
 
-print(select())
+print(select(title=1, ISBN=2))
